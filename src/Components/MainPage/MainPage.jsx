045de366595cdd882319a786bcb5100b040fb59f
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-08-24 18:17:56 +0900

    Update MainPage
    진행중인 도전이 있을 때와 없을 때 화면 표시 구분

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import StartBoard from './Sections/StartBoard';
 
-
 const Layout = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 
-const MainPage = ({ children }) => {
-  const challenge = JSON.parse(localStorage.getItem("challenge"));
-  const { goal, startDate, endDate, reward } = challenge;
-
+const MainPage = () => {
   return (
     <div className='main-page'>
       <Layout>
--- src/Components/MainPage/Sections/StartBoard.jsx
+++ src/Components/MainPage/Sections/StartBoard.jsx
@@ -4,45 +4,57 @@ import styled from 'styled-components';
 import Button from '../../commons/Button';
 import { useDispatch, useSelector } from 'react-redux'
 import { showModal } from '../../../module/challenge';
+import Board from '../../commons/Board';
 
-const Board = styled.div`
-  width: 32%;
-  height: 300px;
-  border-radius: 3px;
-  background-color: #ffffff;
-  border: 1px solid #eeeae6;
-  padding: 1.5rem;
-  margin-bottom: 50px;
+const Challenge = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-size: 1rem;
+  color: #555;
 
-  div {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+  span{
+    font-size: 3.5rem;
   }
-  p {
-    font-size: 1rem;
-    color: #555;
-    margin: 1.2rem 0;
+  em {
+    font-size: 1.5rem;
+    font-weight: 500;
+    color: #333;
+    margin: 1.2rem 0 1rem;
   }
 `;
 
+
 function StartBoard() {
+  const challengeInfo = JSON.parse(localStorage.getItem("challengeInfo")) || null;
   const dispatch = useDispatch();
 
   const onClickStart = () => {
     dispatch(showModal());
   }
+
   return (
-    <Board>
-    <h3>Challenge</h3>
-    <div>
-      <em style={{ fontSize: "3rem"}}>&#128581;</em>
-      <p>아직 진행중인 도전이 없습니다.</p>
-        <Button onClick={onClickStart} title="도전 시작" />
-    </div>
-  </Board>
+    <div style={{ width: '100%' }}>
+      <Board title="Challenge">
+        {challengeInfo 
+        ? <Challenge>
+          <span style={{ fontSize: "3rem"}}>{challengeInfo.category}</span>
+          <em>{challengeInfo.goal}</em>
+          <p style={{ marginBottom: '1rem' }}>{challengeInfo.startDate} - {challengeInfo.endDate}</p>
+          <Link to='challenge'>
+            <Button title="자세히 보기" />
+          </Link>
+        </Challenge>
+
+        : <Challenge>
+          <span>&#128581;</span>
+          <p style={{ margin: '1.2rem 0 1.5rem' }}>아직 진행중인 도전이 없습니다.</p>
+          <Button onClick={onClickStart} width="30%" title="도전 시작" />
+        </Challenge>}
+    </Board>
+  </div>
   )
 }
 
