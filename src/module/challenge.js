e27b8b0c0b6576c3400163429aa88f24258ae03b
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-08-31 20:43:33 +0900

    Add delete challenge action

--- src/module/challenge.js
+++ src/module/challenge.js
@@ -2,6 +2,7 @@ export const SHOW_MODAL = 'SHOW_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const START_CHALLENGE = 'START_CHALLENGE';
 export const EDIT_CHALLENGE = 'EDIT_CHALLENGE';
+export const DELETE_CHALLENGE = 'DELETE_CHALLENGE';
 
 // 액션 생성 함수
 export const showModal = () => {
@@ -25,10 +26,14 @@ export const editChallenge = () => {
   return { type: EDIT_CHALLENGE }
 }
 
+export const deleteChallenge = () => {
+  return { type: DELETE_CHALLENGE }
+}
 
 // 초기 상태
 const initialState = {
   modal: false,
+  initial: true,
   challengeInfo: [],
 };
 
@@ -48,6 +53,7 @@ export default function challenge (state = initialState, action){
     case START_CHALLENGE: 
       return {
         ...state,
+        initial: false,
         challengeInfo: action.payload,
       }
     case EDIT_CHALLENGE: 
@@ -56,6 +62,8 @@ export default function challenge (state = initialState, action){
         modal: true,
         edit: true
       }
+    case DELETE_CHALLENGE: 
+      return initialState;
     default:
       return state;
   }
