9933c63ee896c05633a50f43b5671e139e0b809f
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-09-03 16:30:52 +0900

    Add reset check box action

--- src/module/check.js
+++ src/module/check.js
@@ -1,5 +1,6 @@
 export const CHECK_BOX = 'CHECK_BOX';
 export const CANCEL_BOX = 'CANCEL_BOX';
+export const CHECK_RESET = 'CHECK_RESET';
 
 // 액션 생성 함수
 export const checkBox = (day) => {
@@ -16,6 +17,13 @@ export const cancelBox = (day) => {
   }
 }
 
+export const checkReset = (day) => {
+  return {
+    type: CHECK_RESET,
+    day,
+  }
+}
+
 // 초기 상태
 const initialState = [];
 
@@ -26,6 +34,8 @@ export default function check (state = initialState, action){
       return state.concat(action.day);
     case CANCEL_BOX: 
       return state.filter(day => day !== action.day);
+    case CHECK_RESET: 
+      return initialState;
     default:
       return state;
   }
